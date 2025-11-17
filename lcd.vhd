36d80dc2b4462de6c2c91bc1ade13912cce2e9a3
Erik Branco Queiroz <erikbrancoq@gmail.com>
2025-11-17 11:04:55 -0300

    Segundo commit

--- lcd.vhd
+++ lcd.vhd
@@ -7,10 +7,9 @@
 ------------------------------------------------------------------ 
 
 
-library IEEE; 
-use IEEE.STD_LOGIC_1164.ALL; 
-use IEEE.STD_LOGIC_ARITH.ALL; 
-use IEEE.STD_LOGIC_UNSIGNED.ALL; 
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL; 
 
 
 entity lcd is 
@@ -75,13 +74,13 @@ stIdle --Write data on DB(0)-DB(7)
 -- Signal Declarations and Constants 
 ------------------------------------------------------------------ 
 
-signal clkCount:std_logic_vector(5 downto 0); 
+signal clk_div_count : integer range 0 to 49 := 0; 
 signal activateW:std_logic:= '0'; --Activate Write sequence 
-signal count:std_logic_vector (16 downto 0):= "00000000000000000"; --15 bit 
+signal count: unsigned (16 downto 0):= (others => '0'); --15 bit        
 count variable for timing delays 
 signal delayOK:std_logic:= '0'; --High when count has reached the 
 right delay time 
-signal OneUSClk:std_logic; --Signal is treated as a 1 MHz clock 
+signal oneUS_enable  : std_logic := '0'; 
 signal stCur:mstate:= stPowerOn_Delay; --LCD control state machine 
 signal stNext:mstate; 
 signal stCurW:wstate:= stIdle; --Write control state machine 
@@ -105,7 +104,7 @@ signal LCD_CMDS : LCD_CMDS_T := ( 0 => "00"&X"3C", --Function Set
 2 => "00"&X"01", --Clear Display 
 3 => "00"&X"02", --return home 
 
-4 => "10"&X"4J", --J --------- V 
+4 => "10"&X"4A", --J --------- V 
 5 => "10"&X"4F", --O 
 6 => "10"&X"47", --G --------- C 
 7 => "10"&X"4F", --O --------- E 
@@ -179,11 +178,7 @@ LCD_CMDS(24) <= "10"&X"35" when (contador = 11) else
 "10"&X"31" when (contador = 2) else 
 "10"&X"30" when (contador /= 2); 
 
-gameover <= '1' when ((LCD_CMDS(24) = "10"&X"30") or (LCD_CMDS(18) = "10"&X"4D" 
-and 
-LCD_CMDS(19) = "10"&X"45" and LCD_CMDS(20) = "10"&X"4E" and LCD_CMDS(21) = 
-"10"&X"47" 
-and LCD_CMDS(22) = "10"&X"4F")); 
+gameover <= '1' when ((contador <= 2) or (certo = "11111")); 
 
 LCD_CMDS(4) <= "10"&X"56" when (gameover='1') else "10"&X"4A"; 
 LCD_CMDS(6) <= "10"&X"43" when (gameover='1') else "10"&X"47"; 
@@ -206,51 +201,54 @@ else
 "10"&X"44" when (gameover='1' and LCD_CMDS(24) = "10"&X"30"); 
 LCD_CMDS(13) <= "10"&X"45" when (gameover='1' and LCD_CMDS(24) = "10"&X"30"); 
 
+process (CLK)
+begin
+  if (CLK'event and CLK = '1') then
+    if (clk_div_count = 49) then
+      oneUS_enable  <= '1';
+      clk_div_count <= 0;
+    else
+      oneUS_enable  <= '0';
+      clk_div_count <= clk_div_count + 1;
+    end if;
+  end if;
+end process;
 
--- This process counts to 50, and then resets. It is used to divide the clock 
-signal time. 
-process (CLK, oneUSClk) 
-begin 
-if (CLK = '1' and CLK'event) then 
-clkCount <= clkCount + 1; 
-end if; 
-end process; 
--- This makes oneUSClock peak once every 1 microsecond 
-
-oneUSClk <= clkCount(5); 
 -- This process incriments the count variable unless delayOK = 1. 
-process (oneUSClk, delayOK) 
+process (CLK) 
 begin 
-if (oneUSClk = '1' and oneUSClk'event) then 
-if delayOK = '1' then 
-count <= "00000000000000000"; 
-else 
-count <= count + 1; 
-end if; 
-end if; 
-end process; 
+  if (CLK'event and CLK = '1') then
+    if (oneUS_enable = '1') then -- Ação só ocorre quando o enable está ativo
+      if delayOK = '1' then 
+        count <= (others => '0'); -- Forma mais segura de zerar
+      else 
+        count <= count + 1; 
+      end if; 
+    end if;
+  end if; 
+end process;
 
 --This goes high when all commands have been run 
 writeDone <= '1' when (lcd_cmd_ptr = LCD_CMDS'HIGH + 1) 
 else '0'; 
 --rdone <= '1' when stCur = stWait else '0'; 
 --Increments the pointer so the statemachine goes through the commands 
-process (lcd_cmd_ptr, oneUSClk) 
- 
+process (CLK, rst) 
 begin 
-if (oneUSClk = '1' and oneUSClk'event) then 
-if ((stNext = stInitDne or stNext = stDisplayCtrlSet or stNext = 
-stDisplayClear) and writeDone = '0') then 
-lcd_cmd_ptr <= lcd_cmd_ptr + 1; 
-elsif stCur = stPowerOn_Delay or stNext = stPowerOn_Delay then 
-lcd_cmd_ptr <= 0; 
-elsif writeDone = '1' then 
-lcd_cmd_ptr <= 3; 
-else 
-lcd_cmd_ptr <= lcd_cmd_ptr; 
-end if; 
-end if; 
-end process; 
+  if (rst = '1') then
+    lcd_cmd_ptr <= 0;
+  elsif (CLK'event and CLK = '1') then 
+    if (oneUS_enable = '1') then
+      if ((stNext = stInitDne or stNext = stDisplayCtrlSet or stNext = stDisplayClear) and writeDone = '0') then 
+        lcd_cmd_ptr <= lcd_cmd_ptr + 1;
+      elsif stCur = stPowerOn_Delay or stNext = stPowerOn_Delay then 
+        lcd_cmd_ptr <= 0; 
+      elsif writeDone = '1' then 
+        lcd_cmd_ptr <= 3;
+      end if; 
+    end if;
+  end if; 
+end process;
 
 -- Determines when count has gotten to the right number, depending on the 
 state. 
@@ -260,23 +258,23 @@ or --20050
 (stCur = stFunctionSet_Delay and count = "00000000000110010") or --50 
 (stCur = stDisplayCtrlSet_Delay and count = "00000000000110010") or --50 
 (stCur = stDisplayClear_Delay and count = "00000011001000000") or --1600 
-(stCur = stCharDelay and count = "11111111111111111")) --Max 
+(stCur = stCharDelay and count = "00000000000100101")); --37
 Delay for character writes and shifts 
 --(stCur = stCharDelay and count = "00000000000100101")) --37 This 
 is proper delay between writes to ram. 
 else '0'; 
 
 -- This process runs the LCD status state machine 
-process (oneUSClk, rst) 
+process (CLK, rst) 
 begin 
-if oneUSClk = '1' and oneUSClk'Event then 
-if rst = '1' then 
-stCur <= stPowerOn_Delay; 
-else 
-stCur <= stNext; 
-end if; 
-end if; 
-end process; 
+  if (rst = '1') then 
+    stCur <= stPowerOn_Delay;
+  elsif (CLK'event and CLK = '1') then 
+    if (oneUS_enable = '1') then -- Ação só ocorre quando o enable está ativo
+      stCur <= stNext; 
+    end if;
+  end if; 
+end process;
 
 
 -- This process generates the sequence of outputs needed to initialize and 
@@ -398,16 +396,16 @@ end case;
 end process; 
 
 --This process runs the write state machine 
-process (oneUSClk, rst) 
+process (CLK, rst) 
 begin 
-if oneUSClk = '1' and oneUSClk'Event then 
-if rst = '1' then 
-stCurW <= stIdle; 
-else 
-stCurW <= stNextW; 
-end if; 
-end if; 
-end process; 
+  if (rst = '1') then 
+    stCurW <= stIdle;
+  elsif (CLK'event and CLK = '1') then 
+    if (oneUS_enable = '1') then -- Ação só ocorre quando o enable está ativo
+      stCurW <= stNextW; 
+    end if;
+  end if; 
+end process;
 
 --This genearates the sequence of outputs needed to write to the LCD screen 
 process (stCurW, activateW) 
